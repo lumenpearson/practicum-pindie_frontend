500c7563d1d410bd8bb140e974737d25c61c028a
LexPearson <wa.5.fa.helper@gmail.com>
2024-05-28 19:08:34 +0300

    fix

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3010/api';
+export const BASE_URL = 'https://pindie-api.nomoredomainswork.ru/api';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
